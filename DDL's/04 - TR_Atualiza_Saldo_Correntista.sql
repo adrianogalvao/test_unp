44de31cfa2319808bfe34d7f2cd5870727a8911f
Adriano Galvão <adrianogalvao@agbtecnologia.com.br>
2019-06-23 22:12:27 -0300

    Alteração da Trigger.

--- DDL's/04 - TR_Atualiza_Saldo_Correntista.sql
+++ DDL's/04 - TR_Atualiza_Saldo_Correntista.sql
@@ -1,15 +1,27 @@
-CREATE TRIGGER TR_ATUALIZA_SALDO_CORRENTISTA
+USE TestUnP
+GO
+
+CREATE OR ALTER TRIGGER TR_Atualiza_Saldo_Correntista
    ON Movimentacao
    AFTER INSERT
 AS
 BEGIN 
 	DECLARE
 		@vIdCorrentista Int,
-		@vValor decimal(15,2)
+		@vValor decimal(15,2),
+		@vTipo char(1)
 	
     SELECT @vValor = Valor, @vIdCorrentista = IdCorrentista FROM INSERTED
 
-	UPDATE Correntista SET SaldoFinanceiro =+ @vValor
-	WHERE IdCorrentista = @vIdCorrentista;
+	if @vTipo = 'C'
+	begin
+		UPDATE Correntista SET SaldoFinanceiro =+ @vValor
+		WHERE IdCorrentista = @vIdCorrentista;
+	end
+	else
+	begin
+		UPDATE Correntista SET SaldoFinanceiro =- @vValor
+		WHERE IdCorrentista = @vIdCorrentista;	
+	end;
 END
-GO
+

d0d143c94ad06a8b5637519d098ec18f4d036788
Adriano Galvão <adrianogalvao@agbtecnologia.com.br>
2019-06-24 19:52:35 -0300

    Foreign.

--- DDL's/03 - Constraint_FK_Correntista.sql
+++ DDL's/03 - Constraint_FK_Correntista.sql
@@ -1,6 +1,11 @@
 USE TestUnP
 GO
 
+IF not EXISTS( SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'Movimentacao' AND  COLUMN_NAME = 'IdCorrentista')
+begin
+	ALTER TABLE Movimentacao ADD IdCorrentista Int;
+end;
+
 if OBJECT_ID('FK_Correntista', 'F') is null
 begin
 	ALTER TABLE Movimentacao ADD CONSTRAINT FK_Correntista FOREIGN KEY (IdCorrentista) REFERENCES Correntista (IdCorrentista);
